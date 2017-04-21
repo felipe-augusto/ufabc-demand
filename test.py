0a46523f8dce102054a36087de01fca1e76e6915
Felipe Santos <fesntmail@gmail.com>
2017-04-21 18:33:07 -0300

    Refactor and new attempt
    Refactor the old code into a more readable manner, but the error is so high that we need to try another alternative, test2 will try to solve this by using neural networks

--- test.py
+++ test.py
@@ -4,102 +4,65 @@
 import json
 from pprint import pprint
 from sklearn import linear_model
-from random import randint
+import utils
+import warnings
+from sklearn.preprocessing import PolynomialFeatures
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 clf = linear_model.LinearRegression()
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
+disciplinas = utils.open_data_from_quad('1q2017')
 
-quad = '1q2017'
+training_array = []
+test_array = []
+expected_y_outputs = []
 
-cursos_ids = {
-  'Bacharelado em Ciência da Computação': 16,
-  'Bacharelado em Ciência e Tecnologia': 20,
-  'Bacharelado em Ciências Biológicas': 17,
-  'Bacharelado em Ciências Econômicas': 1,
-  'Bacharelado em Ciências e Humanidades': 22,
-  'Bacharelado em Filosofia': 10,
-  'Bacharelado em Física': 28,
-  'Bacharelado em Matemática': 21,
-  'Bacharelado em Neurociência' : 24,
-  'Bacharelado em Planejamento Territorial': 11,
-  'Bacharelado em Políticas Públicas': 3,
-  'Bacharelado em Química': 14,
-  'Bacharelado em Relações Internacionais': 27,
-  'Engenharia Aeroespacial': 26,
-  'Engenharia Ambiental e Urbana': 6,
-  'Engenharia Biomédica': 18,
-  'Engenharia de Energia': 7,
-  'Engenharia de Gestão': 23,
-  'Engenharia de Informação' : 8,
-  'Engenharia de Instrumentação, Automação e Robótica': 2,
-  'Engenharia de Materiais': 15,
-  'Licenciatura em Ciências Biológicas': 13,
-  'Licenciatura em Física': 25,
-  'Licenciatura em Matemática': 9,
-  'Licenciatura em Química': 4
-}
+# fill training_array, test_array and expected_y_outputs
+for index, i in enumerate(disciplinas):
+	current_disciplina = disciplinas[i]
+	# we need to transform disciplinas inputs to an array
+	current_disciplina_array = []
 
-def transform_dict_to_array(cursos_ids):
-	cursos_ids_array = []
-	for item in cursos_ids:
-		cursos_ids_array.append(cursos_ids[item])
-	return cursos_ids_array
+	# set where inputs will be added
+	utils.add_disciplina_data_to(current_disciplina_array)
+	# add inputs
+	utils.add_campus_value(current_disciplina)
+	utils.add_cr_aluno_value(current_disciplina)
+	utils.add_reprovacoes_value(current_disciplina)
+	utils.add_vagas_value(current_disciplina)
+	utils.add_turno_value(current_disciplina)
+	utils.add_cursos_obrigatorios_value(current_disciplina)
 
-# as the length is 25, we need at least 25 attributes just for the courses !!
-cursos_ids_array = transform_dict_to_array(cursos_ids)
-cursos_ids_array.sort()
-# len(cursos_ids_array)
+	# add desired output
+	current_y = utils.add_ratio_value(current_disciplina)
 
-with open('data/' + quad + '/prepared.json') as data_file:    
-	   data = json.load(data_file)
-
-x = []
-y = []
-
-tests = []
-
-for index, item in enumerate(data):
-	global test_me
-	array_of_datas = []
-	# [campus, cr_aluno, reprovacoes, vagas]
-
-	# first argument is campus -> 1 = Santo Andre, 0 = Sao Bernardo
-	if data[item]['campus'].rfind('anto') != -1:
-		array_of_datas.append(1)
+	# choose between test or training data and append
+	if utils.should_add_to_training_array():
+		training_array.append(current_disciplina_array)
+		expected_y_outputs.append(current_y)
 	else:
-		array_of_datas.append(0)
-	# second argument is cr aluno
-	array_of_datas.append(data[item]['cr_aluno'])
-	# third arguemnt reprovacoes
-	array_of_datas.append(data[item]['reprovacoes'])
-	# fouth argument vagas
-	array_of_datas.append(data[item]['vagas'])
-	# fifth argument turno -> 1 = noturno, 0 = diurno
-	if data[item]['turno'].rfind('not') != -1:
-		array_of_datas.append(1)
-	else:
-		array_of_datas.append(0)
+		current_test = [current_disciplina_array, current_y]
+		test_array.append(current_test)
 
-	# cursos, abrindo em diversos parametros
-	for obrigatoria in cursos_ids_array:
-		if obrigatoria in data[item]['obrigatorias']:
-			array_of_datas.append(1)
-		else:
-			array_of_datas.append(0)
-	
-	if randint(0,12) % 5 != 1:
-		y.append(data[item]['ratio'])
-		x.append(array_of_datas)
-	else:
-		tests.append([array_of_datas, data[item]['ratio']])
+# train
+poly = PolynomialFeatures(degree=1)
+training_array_changed = poly.fit_transform(training_array)
+clf.fit(training_array, expected_y_outputs);
 
-clf.fit(x, y);
+print 'tamanho do vetor de treinamento: ', len(training_array)
+print 'tamanho do vetor de testes: ', len(test_array)
 
-print 'tamanho do vetor de treinamento: ', len(x)
-print 'tamanho do vetor de testes: ', len(tests)
+# number of wrong requisicoes that we are willing to accept
+threshold = 10
+error = 0
 
-for index, test in enumerate(tests):
-	if index > 20:
-		break
-	print 'previsao: ', clf.predict(test[0])[0], '- real: ', test[1]
+for index, test in enumerate(test_array):
+	input = test[0]
+	vagas = input[3]
+	predicao_requisicoes = vagas * clf.predict(input)[0] #poly.fit_transform(input)
+	requisicoes = vagas * test[1]
+	if abs(predicao_requisicoes - requisicoes) > threshold:
+		error += 1
+
+error_percentage = float(error) / len(test_array)
+print error_percentage
---
+++ utils.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import json
+from random import randint
+
+ids_cursos_ufabc =  {
+		'Bacharelado em Ciência da Computação': 16,
+		'Bacharelado em Ciência e Tecnologia': 20,
+		'Bacharelado em Ciências Biológicas': 17,
+		'Bacharelado em Ciências Econômicas': 1,
+		'Bacharelado em Ciências e Humanidades': 22,
+		'Bacharelado em Filosofia': 10,
+		'Bacharelado em Física': 28,
+		'Bacharelado em Matemática': 21,
+		'Bacharelado em Neurociência' : 24,
+		'Bacharelado em Planejamento Territorial': 11,
+		'Bacharelado em Políticas Públicas': 3,
+		'Bacharelado em Química': 14,
+		'Bacharelado em Relações Internacionais': 27,
+		'Engenharia Aeroespacial': 26,
+		'Engenharia Ambiental e Urbana': 6,
+		'Engenharia Biomédica': 18,
+		'Engenharia de Energia': 7,
+		'Engenharia de Gestão': 23,
+		'Engenharia de Informação' : 8,
+		'Engenharia de Instrumentação, Automação e Robótica': 2,
+		'Engenharia de Materiais': 15,
+		'Licenciatura em Ciências Biológicas': 13,
+		'Licenciatura em Física': 25,
+		'Licenciatura em Matemática': 9,
+		'Licenciatura em Química': 4
+}
+
+def open_data_from_quad(quad):
+	with open('data/' + quad + '/prepared.json') as data_file:    
+		data = json.load(data_file)
+	return data
+
+def transform_dict_to_array_and_sort(cursos_ids):
+	cursos_ids_array = []
+	for item in cursos_ids:
+		cursos_ids_array.append(cursos_ids[item])
+	cursos_ids_array.sort()
+	return cursos_ids_array
+
+cursos_ids_sorted_array = transform_dict_to_array_and_sort(ids_cursos_ufabc)
+
+disciplina_data = []
+
+def add_disciplina_data_to(current_array):
+	global disciplina_data
+	disciplina_data = current_array
+
+# 1 = Santo Andre, 0 = Sao Bernardo
+def add_campus_value(disciplina):
+	global disciplina_data
+	if disciplina['campus'].rfind('anto') != -1:
+		disciplina_data.append(1)
+	else:
+		disciplina_data.append(0)
+
+def add_cr_aluno_value(disciplina):
+	global disciplina_data
+	disciplina_data.append(disciplina['cr_aluno'])
+
+def add_reprovacoes_value(disciplina):
+	global disciplina_data
+	disciplina_data.append(disciplina['reprovacoes'])
+
+def add_vagas_value(disciplina):
+	global disciplina_data
+	disciplina_data.append(disciplina['vagas'])
+
+def add_turno_value(disciplina):
+	global disciplina_data
+	if disciplina['turno'].rfind('not') != -1:
+		disciplina_data.append(1)
+	else:
+		disciplina_data.append(0)
+
+def add_cursos_obrigatorios_value(disciplina):
+	global cursos_ids_sorted_array
+	for obrigatoria in cursos_ids_sorted_array:
+		if obrigatoria in disciplina['obrigatorias']:
+			disciplina_data.append(1)
+		else:
+			disciplina_data.append(0)
+
+def should_add_to_training_array():
+	# one in twenty
+	if randint(0,20) % 11 != 1:
+		return True
+	else:
+		return False
+
+def add_ratio_value(disciplina):
+	return disciplina['ratio']
